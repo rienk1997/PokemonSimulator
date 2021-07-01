168bd4334f41babd709bdc29ee90efc0b99a2fe7
Rienk Taborsky <rienkimre@hotmail.nl>
2021-07-01 22:29:19 +0200

    Add project files.

---
+++ PokemonSimulator/MainView.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace PokemonSimulator
+{
+  partial class MainView
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.logBox = new System.Windows.Forms.ListBox();
+      this.SuspendLayout();
+      // 
+      // logBox
+      // 
+      this.logBox.FormattingEnabled = true;
+      this.logBox.Location = new System.Drawing.Point(28, 187);
+      this.logBox.Name = "logBox";
+      this.logBox.Size = new System.Drawing.Size(747, 238);
+      this.logBox.TabIndex = 0;
+      // 
+      // MainView
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.logBox);
+      this.Name = "MainView";
+      this.Text = "Pokemon Battle Simulator";
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.ListBox logBox;
+  }
+}
+
---
+++ PokemonSimulator/MainView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PokemonSimulator
+{
+  public partial class MainView : Form
+  {
+    private static MainView Instance = null;
+
+    public static MainView GetInstance
+    {
+      get
+      {
+        if (Instance == null)
+        {
+          Instance = new MainView();
+        }
+        return Instance;
+      }
+    }
+    private MainView()
+    {
+      InitializeComponent();
+    }
+  }
+}
