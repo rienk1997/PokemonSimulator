79c4bd314af4fee3a6acce2038ca33c98d066fe6
Rienk Taborsky <rienk.taborsky@netivity.nl>
2021-07-29 14:40:30 +0200

    Action Refactor

---
+++ PokemonSimulator/Models/SimulationState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonSimulator.Models
+{
+  public enum SimulationState
+  {
+    Start,
+    Trainer1Turn,
+    Trainer2Turn,
+    Trainer1Win,
+    Trainer2Win
+  }
+}
